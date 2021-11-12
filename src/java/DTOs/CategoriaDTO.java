a032fd34a156ceac77e373d39494f92af9fa8c1f
UnChatNoir1 <87830785+UnChatNoir1@users.noreply.github.com>
2021-11-12 09:58:09 -0300

    Terminados

---
+++ src/java/DTOs/CategoriaDTO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTOs;
+
+import java.io.Serializable;
+
+public class CategoriaDTO implements Serializable {
+    private String nombre;
+    
+    //CONSRUCTOR
+    public CategoriaDTO (String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //GETTERS
+    public String getNombre() {
+        return nombre;
+    }
+    
+    //SETTERS
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ src/java/DTOs/FuncionDTO.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTOs;
+
+import Logica.Clases.Artista;
+import Logica.Clases.Espectaculo;
+import java.io.Serializable;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.Map;
+
+public class FuncionDTO implements Serializable {
+
+        private String nombre;
+        private Date fecha;
+        private Time horaInicio;
+        private Date fechaRegistro;
+        private Espectaculo espectaculo;
+        private Map<String, Artista> artistas;
+
+        private String urlImagen;
+        
+        //CONSRUCTOR
+        public FuncionDTO (String nombre, Date fecha, Time horaInicio, Date fechaRegistro, Espectaculo espectaculo, Map<String, Artista> artistas, String urlImagen){
+            this.nombre= nombre;
+            this.fecha= fecha;
+            this.horaInicio= horaInicio;
+            this.fechaRegistro= fechaRegistro;
+            this.espectaculo=espectaculo;
+            this.artistas=artistas;
+            this.urlImagen=urlImagen;
+        }
+        
+        //GETTERS
+        public String getNombre(){
+            return this.nombre;
+        }
+        
+        public Date getFecha(){
+            return this.fecha;
+        }
+        
+        public Time getHoraInicio(){
+            return this.horaInicio;
+        }
+        
+        public Date getFechaRegistro(){
+            return this.fechaRegistro;
+        }
+        
+        public Map<String, Artista> getArtistas(){
+            return this.artistas;
+        }
+        
+        public Espectaculo getEspectaculo(){
+            return this.espectaculo;
+        }
+        
+        public String getUrlIamgen (){
+            return this.urlImagen;
+        }
+        
+        //SETTERS
+        public void setNombre(String nombre_){
+            this.nombre=nombre_;
+        }
+        
+        public void setFecha(Date fecha_){
+            this.fecha= fecha_;
+        }
+        
+        public void setHoraInicio(Time horaInicio_){
+            this.horaInicio= horaInicio_;
+        }
+        
+        public void setFechaRegistro(Date fechaRegistro_){
+            this.fechaRegistro=fechaRegistro_;
+        }
+        
+        public void setArtistas(Map<String, Artista> artistas){
+            this.artistas=artistas;
+        }
+        
+        public void setUrlImagen (String urlImagen){
+            this.urlImagen=urlImagen;
+        }
+}
